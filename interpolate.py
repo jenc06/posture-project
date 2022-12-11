c249004b7861831257b3d6ce1afd2fc3b304b9fd
jenc06 <120084651+jenc06@users.noreply.github.com>
2022-12-10 17:01:14 -0800

    import libraries and list files

---
+++ interpolate.py
@@ -0,0 +1,8 @@
+import os
+import pandas as pd
+import numpy as np
+from scipy import stats as st
+
+# list files
+file_path = "/Users/jenniferchoi/Documents/ASR/data/test1datasensorfiles/CAmotion/CAsensor/"
+df_list = os.listdir(file_path)
