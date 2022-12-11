aa15a457238ef0f85075cee80fc3f65b4312f1f0
jenc06 <120084651+jenc06@users.noreply.github.com>
2022-12-10 17:02:54 -0800

    removed scipy becsause it is not yet needed

--- interpolate.py
+++ interpolate.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 import numpy as np
-from scipy import stats as st
 
 # list files
 file_path = "/Users/jenniferchoi/Documents/ASR/data/test1datasensorfiles/CAmotion/CAsensor/"
