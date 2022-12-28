c1747973b1b9ed6d4bdc10bd06fb7b75f565ad21
jenc06 <120084651+jenc06@users.noreply.github.com>
2022-12-27 17:43:07 -0800

    added slicing:

---
+++ time_slicing.py
@@ -0,0 +1,17 @@
+from typing import List, Any
+
+import pandas as pd
+import numpy as np
+import csv
+
+interp_df = pd.read_csv("./data/preprocessed/final_interpolated_test.csv", header=None, index_col=False, skiprows=1)
+interp_np = interp_df.to_numpy()
+
+for idx, i in enumerate(range(0,interp_np.shape[0],25)):
+    sliced_name = "./data/sliced/sliced_"+str(idx)+".csv"
+    sliced = interp_np[i:i+25,:]
+
+    sliced_df = pd.DataFrame(sliced)
+    final = sliced_df.to_csv(sliced_name, index=False, header=False)
+    
+    
