67bcb99aa28eb86a25074ebcc0bb6cd1ea454d88
jenc06 <120084651+jenc06@users.noreply.github.com>
2023-01-05 21:50:00 -0800

    Fixed the data folder path

--- time_slicing.py
+++ time_slicing.py
@@ -4,10 +4,12 @@ import pandas as pd
 import numpy as np
 import csv
 
-interp_df = pd.read_csv("./data/preprocessed/final_interpolated_test.csv", header=None, index_col=False, skiprows=1)
+interp_df = pd.read_csv("/Users/jenniferchoi/Git/asr-project-2023/data/preprocessed/final_interpolated_test.csv", header=None, index_col=False, skiprows=1)
 interp_np = interp_df.to_numpy()
 
+
 for idx, i in enumerate(range(0,interp_np.shape[0],25)):
+    print("running")
     sliced_name = "./data/sliced/sliced_"+str(idx)+".csv"
     sliced = interp_np[i:i+25,:]
 
