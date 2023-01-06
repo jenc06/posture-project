524bb6d764937699c9fe42524c054eb3d2247fd5
Jennifer Choi <ladyjchoi@gmail.com>
2023-01-05 20:38:58 -0800

    add file rename utils

---
+++ utils.py
@@ -0,0 +1,17 @@
+import os
+import glob
+
+if __name__ == "__main__":
+    # remove timestamps from all the existing files
+    filenames = glob.glob("./data/good/*.csv") + glob.glob("./data/mild/*.csv") + glob.glob("./data/bad/*.csv")
+    print(filenames[0])
+
+    for file in filenames:
+        folder = os.path.split(file)[0]
+        filename = os.path.split(file)[-1]
+        start_ndx = filename.find('_ts_')
+
+        new_filename = filename[:start_ndx] + filename[start_ndx+22:]
+        new_filename = os.path.join(folder, new_filename)
+
+        os.rename(file, new_filename)
